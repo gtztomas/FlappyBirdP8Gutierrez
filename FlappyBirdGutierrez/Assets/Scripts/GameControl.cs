f7099ee14a2b185ec8c92c205399b3e21988186c
Tomas Gutierrez <723369@student.dusd.net>
2023-12-13 09:56:48 -0800

    Made score text work
    Made my score text work

--- FlappyBirdGutierrez/Assets/Scripts/GameControl.cs
+++ FlappyBirdGutierrez/Assets/Scripts/GameControl.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverText;
+    public TextMeshProUGUI scoreText;
+   
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
+   
+    private int score = 0;
     // Start is called before the first frame update
     void Awake()
     {
@@ -25,10 +31,18 @@ public class GameControl : MonoBehaviour
     { 
         if (gameOver == true && Input.GetMouseButtonDown (0))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
         }
     }
-
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
     public void BirdDied()
     {
         gameOverText.SetActive(true);
